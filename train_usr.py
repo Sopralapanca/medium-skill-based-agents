77da608c307a2ce2965b90e96f50603587b63174
Giacomo Carfi <giacomo.carfi@phd.unipi.it>
2025-12-23 10:50:27 +0000

    fix device train usr

--- train_usr.py
+++ train_usr.py
@@ -12,13 +12,13 @@ _config = {}
 if _config_path.exists():
     with open(_config_path, "r") as f:
         _config = yaml.safe_load(f) or {}
-max_training_steps = _config.get("max_training_steps", 10000)
+max_training_steps = 100000
+#_config.get("max_training_steps", 10000)
 
 all_tr_eps = []
 all_val_eps = []
 ENVS = _config.get("ENVS", [])
-steps_in_get_episodes = 5000 # if 1000 will raise error too few episodes collected
-
+steps_in_get_episodes = 35000
 
 for env_name in ENVS:
     print(f"Collecting episodes from {env_name}...")
@@ -35,7 +35,7 @@ random.shuffle(all_tr_eps)
 random.shuffle(all_val_eps)
 
 state_rep_model = UnsupervisedStateRepresentationModel(
-    observation=all_tr_eps[0][0], max_training_steps=max_training_steps
+    observation=all_tr_eps[0][0], max_training_steps=max_training_steps, device=device
 )
 
 state_rep_model.train(all_tr_eps, all_val_eps)
